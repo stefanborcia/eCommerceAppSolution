a21ea62cb7890dc38bc3685249d1c87ae28a9bf7
stefanborcia <stefigabi89@yahoo.com>
2025-03-14 15:28:51 +0100

    Add Interfaces and Repositories & Generic

---
+++ eCommerceApp.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using eCommerceApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity> : IGeneric<TEntity> where TEntity : class
+    {
+        public Task<int> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
