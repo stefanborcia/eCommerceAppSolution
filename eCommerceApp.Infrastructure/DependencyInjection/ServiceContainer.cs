fdf538bed9f5150cf99741f2a3277b7ab21e1935
stefanborcia <stefigabi89@yahoo.com>
2025-03-16 11:21:16 +0100

    Add Services & ConnectionString

---
+++ eCommerceApp.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,36 @@
+﻿using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Interfaces;
+using eCommerceApp.Infrastructure.Data;
+using eCommerceApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = "DefaultConnection";
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(connectionString),
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName);
+                    sqlOptions.EnableRetryOnFailure(); // Enable automatic retries for transient failures
+                }),
+                ServiceLifetime.Scoped);
+            
+            services.AddScoped<IGeneric<Product>, GenericRepository<Product>>();
+            services.AddScoped<IGeneric<Category>, GenericRepository<Category>>();
+
+            return services;
+        }
+    }
+}
